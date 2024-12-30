104d0d9ea289aaac774db3f9f207239e6dc401d3
sudhamshreddy7 <sudhamshreddy06@gmail.com>
2024-12-29 22:23:46 -0500

    react update

---
+++ front-end/java-react/src/App.js
@@ -0,0 +1,28 @@
+import React,{useState} from "react";
+import './App.css';
+
+function App() {
+  const [activeTab , setActiveTab] = useState('image-generator');
+  const handleTabChange = (tab) => {
+    // alert(tab)
+    setActiveTab(tab)
+  };
+  return (
+    <div className="App">
+    <button className={activeTab === 'image-generator' ? 'active' : ''}
+        onClick={()=> handleTabChange('image-generator')}>Generate Image</button>
+      <button className={activeTab === 'chat' ? 'active' : ''}
+          onClick={() => handleTabChange('chat')}>Chat</button>
+      <button className={activeTab === 'recipe-generator' ? 'active' : ''}
+          onClick={()=> handleTabChange('recipe-generator')}>Recipe Generator</button>
+    <div>
+      {activeTab === 'image-generator' && <h2>Image Generator</h2>}
+      {activeTab === 'chat' && <h2>Chat</h2>}
+      {activeTab === 'recipe-generator' && <h2>Recipe Generator</h2>}
+    </div>
+    </div>
+
+  );
+}
+
+export default App;
